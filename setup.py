6c8bc0ba47c6c0f6dc6d588198d84e43bf7a39fb
sonamproject-2025 <sonamproject2025@gmail.com>
2025-04-26 10:24:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Low bandwidth DoS tool. Slowloris rewrite in Python.",
     author="Sonam",
     author_email="sonamproject2025@gmail.com",
-    url="https://github.com/gkbrk/slowloris",
+    url="https://github.com/sonamproject-2025/DDoS-Attack/blob/main/slowloris.py",
     keywords=["dos", "http", "slowloris"],
     license="MIT",
 )
